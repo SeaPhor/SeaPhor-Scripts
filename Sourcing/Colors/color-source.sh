556a5a2ce86d2861a5bcb1428daf0da632821566
SeaPhor <seaphor@woodbeeco.com>
2018-10-07 11:56:40 -0500

    Script Auto Run

--- Sourcing/Colors/color-source.sh
+++ Sourcing/Colors/color-source.sh
@@ -2,27 +2,33 @@
 #
 ###	You can source this file to add colors to your output text
 #
-#####################################################################
-#####	Setting color variables
-#####################################################################
+###########################################
+####    Declare Color Variables
+###########################################
 #
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
-LTBLK=`tput setaf 8`
-LTRED=`tput setaf 9`
-LTGRN=`tput setaf 10`   
-LTYLLW=`tput setaf 11`   
-LTBLU=`tput setaf 12`   
-LTMAG=`tput setaf 13`   
-LTCYN=`tput setaf 14`   
-LTWHT=`tput setaf 15`   
-
-BOLD=`tput bold`
-RESET=`tput sgr0`
+[[ $@ =~ "nocolor" ]] && NOCOLR=true || NOCOLR=false
+#
+if ! $NOCOLR; then
+    RED="$(tput setaf 1)"
+    GRN="$(tput setaf 2)"
+    YLLW="$(tput setaf 3)"
+    BLU="$(tput setaf 4)"
+    MAG="$(tput setaf 5)"
+    CYN="$(tput setaf 6)"
+    LTRED="$(tput setaf 9)"
+    LTGRN="$(tput setaf 10)"
+    LTYLLW="$(tput setaf 11)"
+    LTBLU="$(tput setaf 12)"
+    LTMAG="$(tput setaf 13)"
+    LTCYN="$(tput setaf 14)"
+    BGBLU="$(tput setab 4)"
+    BGYLLW="$(tput setab 3)"
+    BGLYLLW="$(tput setab 11)"
+    ULINE="$(tput smul)"
+    NULINE="$(tput rmul)"
+    SOMODE="$(tput smso)"
+    NSOMODE="$(tput rmso)"
+    BOLD="$(tput bold)"
+    RESET="$(tput sgr0)"
+fi
 #
