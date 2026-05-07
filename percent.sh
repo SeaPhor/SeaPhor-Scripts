c2f84d05cff34e2acf9c9e1276190563eeb9b1d0
SeaPhor <woodbeeco.owner@gmail.com>
2026-05-07 15:41:37 -0500

    adding percents

---
+++ percent.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+structure () {
+    cat <<EOT
+    Structure
+     n      x
+    ---    ---
+     a     100
+EOT
+}
+echo ""
+structure
+echo ""
+
+declare -i p=100
+read -p "Which value are you wanting? n|x " q
+if [[ $q == "n" ]]; then
+    read -p "Ok then what is the 'x' value? " x
+else
+    read -p "Ok then what is the 'n' value? " n
+fi
+read -p "And what is the mass amount you need the percetage of?? " a
+
+for f in n x a ; do declare -i $f=$f; done
+
+if [[ $q == "n" ]]; then
+    p1="$(expr $x \* $a)"
+    n="($p1 / $p)"
+    echo -e "\nYour value is $n"
+else
+    p1="$(expr $n \* $p)"
+    x="($p1 / $a)"
+    echo -e "\nYour value is $x%"
+fi
+exit 0
